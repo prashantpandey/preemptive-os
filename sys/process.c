20f97b24324426a8279a0c009d4799f1e8b93f40
Prashant Pandey <ppandey@compute-2-30.local>
2013-11-16 03:08:55 -0500

    Kernel context switch working

--- sys/process.c
+++ sys/process.c
@@ -3,13 +3,13 @@
 #include <stdio.h>
 #include <paging.h>
 
-
-task readyQ[10];
-
 task thread1;
 task thread2;
 
 int num_process = 2;
+task readyQ[5]; 
+
+bool flag = true;
 
 void function1();
 void function2();
@@ -19,11 +19,11 @@ void first_context_switch() {
 	// set the address of function1 to the start of the stack of thread1 and same for thread2
 	// set the rsp to point to the stack
 
-	int i = 0;
-	for(i = 0; i < 1024; i++) {
-		thread1.stack[i] = 0;
-		thread2.stack[i] = 0;
-	}
+	//int i = 0;
+	//for(i = 0; i < 1024; i++) {
+	//	thread1.stack[i] = 0;
+	//	thread2.stack[i] = 0;
+	//}
  
 	thread1.stack[63] = (uint64_t)&function1;	
 	thread1.rsp = (uint64_t)&thread1.stack[59];		
@@ -57,7 +57,6 @@ void switch_to(task* prev, task* next) {
 	// will update the value of the current rsp to the point to the rsp of the next task
 	// this will cause the context switch from prev task to next task
 	// TODO: write the assembly language code
-	do {
 	__asm__ __volatile__ (
 			"pushq %rax;"\
 			"pushq %rbx;"\
@@ -80,27 +79,33 @@ void switch_to(task* prev, task* next) {
 			"popq %rax;"
 	);	
     	asm volatile("retq");
-	} while(0);
 }
 
-void schedule(int i) {
+void schedule() {
 	// will halt the currently executing thread
 	// will pop the next task from the ready queue
 	// call switch_to function with prev and next task
-	if(i == 0) {	
+	
+	if(flag) {
+		flag = false;
 		switch_to(&readyQ[0], &readyQ[1]);
 	}
-	else if(i == 1) {
+	else {
+		flag = true;
 		switch_to(&readyQ[1], &readyQ[0]);
 	}
 }
 
 void function1() {
-	printf("Hello ");
-	schedule(0);
+	while(1) {
+		printf("\nHello ");
+		schedule();
+	}
 }
 
 void function2() {
-	printf("World..!!\n");
-	schedule(1);
+	while(1) {
+		printf("World..!!");
+		schedule();
+	}
 }
